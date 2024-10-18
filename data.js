584b06ec4d77563dae08e0d7ff5c9df8aa4b5f48
Ces Tumampos <164609620+cess2c@users.noreply.github.com>
2024-10-18 20:06:03 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const dishes = [
+    {
+      id: 1, 
+      type: 'Sisig',
+      province: 'Pampanga',
+      price: 220,
+      size: "platter",
+      img: "src/sisig.jpg",
+    },
+    {
+      id: 2,
+      type: 'Salpicao',
+      province: 'Quezon',
+      price: 180,
+      size: "platter",
+      img: "src/bagnet.jpg",
+    },
+    {
+      id: 3,
+      type: 'Bagnet',
+      province: 'Ilocos',
+      price: 370,
+      size: "platter",
+      img: "src/salpicao.jpg",
+    }
+];
+module.exports = dishes;
